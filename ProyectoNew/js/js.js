3f1da3676222849a6900ea44f194b6e9ab0d1a7c
aitor <aitorjob69@gmail.com>
2024-03-13 11:05:23 +0100

    js

---
+++ ProyectoNew/js/js.js
@@ -0,0 +1,59 @@
+// if (new Date().getHours()<18) {
+//     document.getElementById('demo').innerHTML="buenos dias";
+// }
+
+
+
+// let sueldo;
+// sueldo = parseFloat(prompt('ingrese su sueldo: '));
+// if(sueldo >= 3000) {
+//     alert('como su sueldo es mayor de ' + 3000 + ' ' + 'debe pagar impuestos');
+// } else {
+//     alert('como su sueldo es menor de ' + 3000 + ' ' + ' no debe pagar impuestos');
+// }
+
+
+
+// let num1, num2;
+// num1 = parseInt(prompt('ingrese un numero:'));
+// num2 = parseInt(prompt('ingrese el segundo numero:'));
+// if (num1 >= num2) {
+//     alert('el mayor es: ' + num1);
+// } else {
+//     alert('el mayor es: ' + num2);
+// }
+
+
+// let num1, num2;
+// num1 = parseFloat(prompt('ingrese un numero:'));
+// num2 = parseFloat(prompt('ingrese el segundo numero:'));
+
+// if (num1 > num2) {
+//     let suma, diferencia;
+//     suma= num1 + num2;
+//     diferencia = num1 + num2;
+//     document.write('la suma es: ' + suma + '\n ' + 'la diferencia es: ' + diferencia);
+// } else {
+//    let producto, division;
+//    producto = num1 * num2
+//    division = num1 / num2
+//    document.write('el producto es; ' + producto + '\n ' + 'la division es: ' + division );
+// }
+
+
+// let num1, num2, num3;
+// num1 = parseFloat(prompt('ingrese el primer número:'));
+// num2 = parseFloat(prompt('ingrese el segundo número:'));
+// num3 = parseFloat(prompt('ingrese el tercer número:'));
+
+// if (num1 > num2 ) {
+//     document.write('el mayor de los tres es el : ' + num1);
+// } else {
+//     if (num2 > num3 ) {
+//         document.write(' el mayor de los tres es el ' + num2);
+//     } else {
+//         document.write('el mayor de los tres es el : ' + num3);
+//     }
+// }
+
+
